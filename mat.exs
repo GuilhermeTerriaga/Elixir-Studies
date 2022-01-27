4defa660dcd81db4aad2ccb76cf3c87a20587768
GuilhermeT <g.terriaga@wisetrust.ai>
2022-01-27 20:58:51 -0300

    Math module created

---
+++ mat.exs
@@ -0,0 +1,9 @@
+defmodule MeuModulo.Math do
+  def soma(param1, param2) do
+    param1 + param2
+  end
+
+  def soma_one_line(param1, param2), do: param1 + param2
+
+  def multiplica(param1, param2), do: param1 * param2
+end
