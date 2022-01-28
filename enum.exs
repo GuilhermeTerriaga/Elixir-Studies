ba2cee5443742a32c48a6ddf9b5f00391df6cddb
GuilhermeT <g.terriaga@wisetrust.ai>
2022-01-27 21:20:36 -0300

    now with both

--- enum.exs
+++ enum.exs
@@ -1,5 +1,5 @@
 defmodule MeuModulo.Enum do
-  def primeiro(lista) do
+  def primeiro_unless(lista) do
     unless length(lista) == 0 do
       hd(lista)
     end
@@ -8,5 +8,13 @@ defmodule MeuModulo.Enum do
     #   nil
     # else
     #   hd(lista)
+  end
+
+  def primeiro_if(lista) do
+    if length(lista) == 0 do
+      nil
+    else
+      hd(lista)
+    end
   end
 end
