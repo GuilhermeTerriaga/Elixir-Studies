eac21e8f7c054f58773c7eb1064666c3e3475953
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2022-02-06 23:19:23 -0300

    guard clause added to Math.exs

--- math.exs
+++ math.exs
@@ -6,4 +6,7 @@ defmodule MeuModulo.Math do
   def soma_one_line(param1, param2), do: param1 + param2
 
   def multiplica(param1, param2), do: param1 * param2
+
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
 end
