817f77b07ac2312840053505538841f3f3516e60
GuilhermeT <g.terriaga@wisetrust.ai>
2022-04-08 16:17:16 -0300

    tabuada with guard clause and more elegant form

--- loop.exs
+++ loop.exs
@@ -4,11 +4,22 @@ defmodule MeuModulo.Loop do
     tabuada(multiplicador, resultado, 1)
   end
 
+  def calcula(multiplicador) when is_integer(multiplicador) and multiplicador >= 0 do
+    tabuada2(multiplicador, 1)
+  end
+
   defp tabuada(_, resultado, 11) do
-    List.to_string(resultado)
+    # List.to_string(resultado)
+    resultado
   end
 
   defp tabuada(produto1, resultado, produto2) do
     tabuada(produto1, List.insert_at(resultado, -1, produto1 * produto2), produto2 + 1)
+  end
+
+  defp tabuada2(_, 11), do: []
+
+  defp tabuada2(produto1, produto2) do
+    [produto1 * produto2] ++ tabuada2(produto1, produto2 + 1)
   end
 end
